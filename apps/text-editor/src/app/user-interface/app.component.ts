406821cc6f1a12915de9a3fc945da76e5096db0f
Gabriel <gabriel@hipcomp.com.br>
2020-02-14 15:52:16 -0300

    setar negrito funcionando

--- apps/text-editor/src/app/user-interface/app.component.ts
+++ apps/text-editor/src/app/user-interface/app.component.ts
@@ -10,22 +10,40 @@ import { Message } from '@text-editor/api-interfaces';
 export class AppComponent{
 
   textoSelecionado: string;
+  elementoSelecionado: string;
 
 
-
-  pegarTextoSelecionado() {
+  pegarTextoSelecionado(event) {
     const texto = window.getSelection();
     this.textoSelecionado = texto.toString();
+    this.elementoSelecionado = event.path[0];
+    console.log(this.elementoSelecionado);
+    console.log(this.textoSelecionado)
+
   }
 
   setarNegrito(){
 
+    // const elemento = document.querySelector('.flex-item h1').style.fontWeight = 'bold';
+    // console.log(document.querySelector('.flex-item h1'));
 
-    const elemento = document.querySelector('.flex-item h1');
+    // console.log(this.elementoSelecionado.valueOf().innerHTML);
+    // console.log(this.textoSelecionado);
+    if(this.elementoSelecionado.style.fontWeight === 'bold') {
+       //this.elementoSelecionado.style.fontWeight = 300;
+      this.elementoSelecionado.style.fontWeight = 300;
+    }
+    else {
+      this.elementoSelecionado.style.fontWeight = 'bold';
+    }
+    //console.log(this.elementoSelecionado.innerHTML.charAt(1));
+   // console.log(this.elementoSelecionado.innerHTML.charAt(2).fontcolor("blue"));
 
-
-    //console.log(this.textoSelecionado);
+    //this.elementoSelecionado.style.fontWeight = 'bold';
   }
 
+  setarItalico() {
+
+  }
 
 }
